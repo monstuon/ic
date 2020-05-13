d4bcec59d78ce297044883869f0d19005aeaa80a
monstuon <eldani.mdq@gmail.com>
2020-05-12 21:01:44 -0300

    setup2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="sugeno-pkg-monstuon", # Replace with your own username
+    name="ic-pkg-monstuon", # Replace with your own username
     version="0.0.1",
     author="Daniel Albornoz",
     author_email="eldani.mdq@gmail.com",
